91076ff26ba068dbc443288e829e279b4d54171e
thearnavgarg <thearnavgarg@gmail.com>
2016-12-22 23:03:56 -0600

    Initial Commit

---
+++ Trees/MainTree.java
@@ -0,0 +1,16 @@
+package Trees;
+
+import java.util.ArrayList;
+
+public class MainTree {
+
+    public static void main(String[] args) {
+
+        TestCases testCases = new TestCases();
+
+        ArrayList<Node> bstRootList = testCases.getBtRootList();
+        ArrayList<Node> treeRootList = testCases.getTreeRootList();
+
+        
+    }
+}
---
+++ Trees/Node.java
@@ -0,0 +1,18 @@
+package Trees;
+
+/**
+ * Created by arnav on 12/22/16.
+ */
+public class Node {
+
+    public int data;
+    public Node left;
+    public Node right;
+
+    public Node(int data, Node left, Node right) {
+
+        this.data = data;
+        this.left = left;
+        this.right = right;
+    }
+}
