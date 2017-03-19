a624d76e8ec88ce7d51d3c08710589ed05e09174
thearnavgarg <thearnavgarg@gmail.com>
2017-03-19 03:28:34 -0500

    starting with c++

---
+++ C++/tree_questions.cc
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <vector>
+
+int main(void) {
+
+    std::cout << "Hello, World!" << std::endl;
+    return 1;
+}
---
+++ C++/tree_questions.h
@@ -0,0 +1,4 @@
+#include <iostream>
+#include <vector>
+
+// Will place all the function names here. 
