f86bb1693e029ddae4fee7fb3957a4dbef22d880
thearnavgarg <thearnavgarg@gmail.com>
2017-11-02 14:15:17 -0500

    Added some comments on the code

--- Practice/C++/arrays/sumInRange.cc
+++ Practice/C++/arrays/sumInRange.cc
@@ -5,14 +5,23 @@
 int sumInRange(std::vector<int> nums, std::vector<std::vector<int>> queries) {
   long long int sum = 0;
   std::vector<int> subsetSum;
+  // Going through the nums vector and adding all the elements till that index
+  // and storing it in the subsetSum vector..
+  // For example
+  // nums = {1, 2, 3, 4, 5}
+  // then subsetSum = {1, 3, 6, 10, 15}
   for (std::vector<int>::iterator it = nums.begin(); it != nums.end(); it++) {
       sum = sum + *it;
       subsetSum.push_back(sum);
   }
   sum = 0; // Resetting the sum value.
+  // Here I am iterating over each set of element in the queries vector.
   for (auto& row : queries) {
+      // row = [init_index, final_index]
       int first_index = row[0];
       int second_index = row[1];
+      // sum(a, b) is sum of elements from index a to b
+      // sum(a, b) = sum(0, b) - sum(0, a) + element.at(a);
       sum = sum + (subsetSum.at(second_index) - subsetSum.at(first_index) + nums.at(first_index));
   }
   unsigned long int num_mod = (int)(std::pow(10, 9) + 7);
