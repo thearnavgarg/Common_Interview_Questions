5f89d0ec667e4c0302ede470ac410496f8bf786d
thearnavgarg <thearnavgarg@gmail.com>
2017-11-03 15:44:15 -0500

    Added one way to solve pal_permu

---
+++ CCTI/c++/arrays/palindrome_permutation.cc
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+
+bool solution(std::string input) {
+  // Assuming that an empty string will return true.
+  if (input.size() < 2) {
+    return true;
+  }
+  char* charMap = new char[256];
+  int countOdd = 0;
+  for (auto& ch : input) {
+    if (ch == ' ') {
+      continue;
+    }
+    ch = std::tolower(ch, std::locale());
+    charMap[ch] += 1;
+    if (charMap[ch] % 2 == 0) {
+      countOdd -= 1;
+    } else {
+      countOdd += 1;
+    }
+  }
+
+  if (countOdd > 1) {
+    return false;
+  }
+  return true;
+}
+
+int main() {
+  std::string input("Tac Coa");
+  std::cout << solution(input) << std::endl;
+  return 0;
+}
