4b0f2fe2f6936b711f84131ae00c0c35f881aaf9
thearnavgarg <thearnavgarg@gmail.com>
2017-11-15 14:16:57 -0600

    Added group anagram solution

---
+++ Practice/C++/arrays/group_anagram.cc
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <map>
+
+void solution(std::string inputList[], int size) {
+  std::map<std::string, std::vector<std::string>> anagram_group;
+
+  for (int i = 0; i < size; i++) {
+    std::string key = inputList[i];
+    std::sort(key.begin(), key.end());
+    if (anagram_group.find(key) != anagram_group.end()) {
+      anagram_group[key].push_back(inputList[i]);
+    } else {
+      std::vector<std::string> temp;
+      temp.push_back(inputList[i]);
+      anagram_group.insert({key, temp});
+    }
+  }
+  std::map<std::string, std::vector<std::string>>::const_iterator it;
+  for (it = anagram_group.begin(); it != anagram_group.end(); it++) {
+    std::vector<std::string> tempVec = it->second;
+    for (std::string s : tempVec) {
+      std::cout << s << " ";
+    }
+    std::cout << std::endl;
+  }
+}
+
+int main() {
+  std::string listAnagram[] = {"eat", "tea", "tan", "ate", "nat", "bat"};
+  solution(listAnagram, 6);
+  return 0;
+}
