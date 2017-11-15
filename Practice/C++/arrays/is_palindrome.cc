c124e65ad8efd2444b4a37a58c8f2475052ebd12
thearnavgarg <thearnavgarg@gmail.com>
2017-11-15 13:20:12 -0600

    added solution for is_palindrome

---
+++ Practice/C++/arrays/is_palindrome.cc
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+
+bool solution(std::string& input) {
+  if (input.empty()) {
+    return false;
+  }
+  if (input.size() == 1) {
+    return true;
+  }
+  std::string cleanString("");
+  for (int i = 0; i < input.size(); i++) {
+    if (std::isalnum(input[i])) {
+      cleanString += std::tolower(input[i]);
+    }
+  }
+
+  int start = 0, end = cleanString.size()-1;
+  while(end >= start) {
+    if (cleanString[start] != cleanString[end]) {
+      return false;
+    }
+    start++; end--;
+  }
+  return true;
+}
+
+int main() {
+  std::string testcase1 = "A man, a plan, a canal: Panama";
+  std::string testcase2 = "race a car";
+
+  std::cout << solution(testcase1) << std::endl;
+  std::cout << solution(testcase2) << std::endl;
+
+  return 0;
+}
