6c3bd8773e624da7d585b2581ea72fb8f810d347
thearnavgarg <thearnavgarg@gmail.com>
2018-04-08 17:08:34 -0500

    Changed from tabs to spaces

--- Practice/Python/trees/traversal.py
+++ Practice/Python/trees/traversal.py
@@ -13,30 +13,30 @@ def inorder(root):
 
 
 def preorder(root):
-	if not root:
-		return None
-	print(root.data)
-	preorder(root.left)
-	preorder(root.right)
+    if not root:
+        return None
+    print(root.data)
+    preorder(root.left)
+    preorder(root.right)
 
 
 def postorder(root):
-	if not root:
-		return None
-	postorder(root.left)
-	postorder(root.right)
-	print(root.data)
+    if not root:
+        return None
+    postorder(root.left)
+    postorder(root.right)
+    print(root.data)
 
 
 def level_order(root):
-	if not root:
-		return None
-	q = Queue()
-	q.put(root)
-	while not q.empty():
-		node = q.get()
-		print(node.data)
-		if node.left:
-			q.put(node.left)
-		if node.right:
-			q.put(node.right)
+    if not root:
+        return None
+    q = Queue()
+    q.put(root)
+    while not q.empty():
+        node = q.get()
+        print(node.data)
+        if node.left:
+            q.put(node.left)
+        if node.right:
+            q.put(node.right)
