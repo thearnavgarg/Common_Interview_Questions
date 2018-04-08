617fa2234bc997d5405e1c98fe789261ee00c6ba
thearnavgarg <thearnavgarg@gmail.com>
2018-04-08 17:06:37 -0500

    Changed some dir names and got some files

---
+++ Practice/Python/trees/traversal.py
@@ -0,0 +1,42 @@
+'''
+Code Implementation of Inorder, Postorder and Preorder
+
+'''
+from queue import Queue
+
+def inorder(root):
+    if not root:
+        return None
+    inorder(root.left)
+    print(root.left)
+    inorder(root.right)
+
+
+def preorder(root):
+	if not root:
+		return None
+	print(root.data)
+	preorder(root.left)
+	preorder(root.right)
+
+
+def postorder(root):
+	if not root:
+		return None
+	postorder(root.left)
+	postorder(root.right)
+	print(root.data)
+
+
+def level_order(root):
+	if not root:
+		return None
+	q = Queue()
+	q.put(root)
+	while not q.empty():
+		node = q.get()
+		print(node.data)
+		if node.left:
+			q.put(node.left)
+		if node.right:
+			q.put(node.right)
